8a05b8ca6f7bd33315f2fe593d72f1cf073463fe
Hao Ling <futurelabs@outlook.com>
2017-07-19 22:10:13 +0800

    Review

---
+++ CSharpReview/BoxAndUnBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpReview
+{
+    class BoxAndUnBox
+    {
+
+        public void Box()
+        {
+            int i = 1;
+
+            object o = i;
+        }
+
+        public void UnBox()
+        {
+            object o = 21;
+
+            int i = (int)o;
+        }
+    }
+}
