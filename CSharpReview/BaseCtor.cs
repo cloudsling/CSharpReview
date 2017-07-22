872c5dc1bbcccf2581f41d70ac8321bd209cfd7b
Hao Ling <futurelabs@outlook.com>
2017-07-22 21:05:00 +0800

    Review

---
+++ CSharpReview/BaseCtor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpReview
+{
+    class BaseCtor
+    {
+        public BaseCtor(double accu)
+        {
+            _name = "Base Constructor";
+            _accul = accu;
+        }
+
+        private string _name;
+
+        private int _count;
+
+        private double _accul;
+    }
+
+    class SubClass : BaseCtor
+    {
+        public SubClass(int count, double acc) : base(acc)
+        {
+            _otherCount = count;
+        }
+
+        private int _otherCount;
+    }
+}
---
+++ CSharpReview/ThisCtor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpReview
+{
+    class ThisCtor
+    {
+        private int _count;
+
+        private string _name;
+
+        private double _accul;
+
+        public ThisCtor()
+        {
+            _count = 1;
+            _name = "This is a string";
+            _accul = 4.2;
+        }
+
+        public ThisCtor(int count) : this()
+        {
+            _count = count;
+        }
+
+        // Other ctors
+    }
+}
