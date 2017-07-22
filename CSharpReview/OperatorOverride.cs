96d62cceeea94cce3853ba3e886660490fa2f00c
Hao Ling <futurelabs@outlook.com>
2017-07-22 21:29:08 +0800

    Review

---
+++ CSharpReview/OperatorOverride.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Security;
+using System.Text;
+
+namespace CSharpReview
+{
+    class OperatorOverride
+    {
+        public int Id { get; set; }
+        public int Tag { get; set; }
+        public string Name { get; set; }
+
+        public static OperatorOverride operator +(OperatorOverride thisObj, OperatorOverride other)
+        {
+            return new OperatorOverride
+            {
+                Id = thisObj.Id + other.Id,
+                Tag = thisObj.Tag * other.Tag,
+                Name = thisObj.Name
+            };
+        }
+    }
+}
--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -8,14 +8,18 @@ namespace CSharpReview
         static void Main(string[] args)
         {
             //MyClass cla = new MyClass();
-
             //cla.ChangeValue(23);
 
+            //BaseClass cls = new SubClass();
+            //cls.Method1();
+            //cls.Method2();
+
+            OperatorOverride oper1 = new OperatorOverride { Id = 12, Tag = 32, Name = "Ling" };
+            OperatorOverride oper2 = new OperatorOverride { Id = 12, Tag = 32, Name = "Ling" };
+            var obj = oper2 + oper1;
+            Console.WriteLine(obj.Name + obj.Tag);
 
 
-            BaseClass cls = new SubClass();
-            cls.Method1();
-            cls.Method2();
             Console.Read();
         }
     }
