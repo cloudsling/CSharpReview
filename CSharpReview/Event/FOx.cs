de243b347f5c068697196e2f4ad5b0386d44bb4a
Hao Ling <futurelabs@outlook.com>
2017-07-24 13:31:14 +0800

    Review

---
+++ CSharpReview/Event/FOx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpReview.Event
+{
+    internal sealed class Fox
+    {
+        public Fox(MailManager manager)
+        {
+            manager.NewMail += FaxMsg;
+        }
+
+        public void FaxMsg(object sender, NewMailEventArgs e)
+        {
+            // TODO: handle when new mail comes
+        }
+
+        public void UnRegister(MailManager manager)
+        {
+            manager.NewMail -= FaxMsg;
+        }
+    }
+}
---
+++ CSharpReview/Event/NewMailEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpReview.Event
+{
+    public class NewMailEventArgs : EventArgs
+    {
+        private readonly string _from, _to, _subject;
+
+        public NewMailEventArgs(string from, string to, string subject)
+        {
+            _from = from;
+            _to = to;
+            _subject = subject;
+        }
+
+        public string From => _from;
+
+        public string To => _to;
+
+        public string Subject => _subject;
+    }
+}
