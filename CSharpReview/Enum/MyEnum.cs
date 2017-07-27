2f957f89ab441db1d4c80c6e4f17f619ff57e545
Hao Ling <futurelabs@outlook.com>
2017-07-27 20:55:17 +0800

    Review

---
+++ CSharpReview/Enum/MyEnum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpReview.Enum
+{
+    internal enum MyEnum : byte
+    {
+        White = 0,
+        Red,
+        Yellow,
+        Green,
+        Blue
+    }
+
+    class EnumTest
+    {
+        public static void Test()
+        {
+            var arr = System.Enum.GetValues(typeof(MyEnum));
+
+            var arr2 = GetEnumValues<MyEnum>();
+        }
+
+        public static TEnum[] GetEnumValues<TEnum>() where TEnum : struct
+        {
+            return (TEnum[])System.Enum.GetValues(typeof(TEnum));
+        }
+    }
+}
