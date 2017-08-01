4945fa18f1fe126a648d12ef72cb64def692eb14
Hao Ling <futurelabs@outlook.com>
2017-08-01 21:17:23 +0800

    Review

---
+++ CSharpReview/ReflectionTest/ReflectionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CSharpReview.ReflectionTest
+{
+    internal class ReflectionTest
+    {
+        public static void Test(object obj)
+        {
+            var obj1 = (TargetObject)CreateInstanceWithMethod(obj.GetType(), 12, "Schrodinger");
+            var obj2 = (TargetObject)CreateInstanceByConstructor(obj.GetType(), 10, "Erwin");
+
+            obj1.DoOtherThing();
+            obj2.DoSomething();
+        }
+
+
+        public static object CreateInstanceByConstructor(Type ty, params object[] args)
+        {
+            ConstructorInfo[] ctorInfo = ty.GetConstructors();
+
+            foreach (var info in ctorInfo)
+            {
+                if (info.GetParameters() != null)
+                {
+                    return info.Invoke(args);
+                }
+            }
+            return new object();
+        }
+
+        public static object CreateInstanceWithMethod(Type ty, params object[] args)
+        {
+            return Activator.CreateInstance(ty);
+        }
+    }
+}
---
+++ CSharpReview/ReflectionTest/TargetObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpReview.ReflectionTest
+{
+    internal class TargetObject
+    {
+        public TargetObject() { }
+
+        public TargetObject(int i, string str)
+        {
+            Id = i;
+            Name = str;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing Something");
+        }
+
+        public void DoOtherThing()
+        {
+            Console.WriteLine("Doing other thing......");
+        }
+    }
+}
