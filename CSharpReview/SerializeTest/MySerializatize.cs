8dc249e326b5c6e22c43cc300f8760d8aaa300f4
Hao Ling <futurelabs@outlook.com>
2017-08-02 17:17:02 +0800

    Review

---
+++ CSharpReview/SerializeTest/MySerializatize.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace CSharpReview.SerializeTest
+{
+    internal class MySerializatize
+    {
+        public void Test()
+        {
+            // .net core 已经移除对二进制序列化的支持，其他包括
+            // 数据契约序列化、XML序列化、JSON.NET以及protobuf-net；
+        }
+
+    }
+
+    [Serializable]
+    public class SerTest
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+
+}
