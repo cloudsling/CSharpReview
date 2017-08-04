511fa46a2d1d2443fa85585be1e5a2433dc2778f
Hao Ling <futurelabs@outlook.com>
2017-08-04 21:27:20 +0800

    Review

--- CSharpReview/ThreadTest/ParallelDemo.cs
+++ CSharpReview/ThreadTest/ParallelDemo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpReview.ThreadTest
@@ -31,6 +33,36 @@ namespace CSharpReview.ThreadTest
         public void DoSomething(int index)
         {
             // Do something
+        }
+
+        public static Int64 DirectoryBytes(String path, String searchPattern, SearchOption option)
+        {
+            var files = Directory.EnumerateFiles(path, searchPattern, option);
+            Int64 masterTotal = 0;
+
+            ParallelLoopResult result = Parallel.ForEach<String, Int64>(
+                    files,
+                    () => 0,
+                    (file, loopState, index, taskLocalTotal) =>
+                    {
+                        Int64 fileLength = 0;
+                        FileStream fs = null;
+                        try
+                        {
+                            fs = File.OpenRead(file);
+                            fileLength = fs.Length;
+                        }
+                        catch (IOException) { }
+                        finally
+                        {
+                            fs?.Dispose();
+                        }
+                        return taskLocalTotal + fileLength;
+                    },
+                    taskLocalTotal => Interlocked.Add(ref masterTotal, taskLocalTotal)
+                );
+
+            return masterTotal;
         }
     }
 }
